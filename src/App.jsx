11f89f177c9d7b2f1ecae042f8897f15df673809
kaito02050620 <suzuki.engineer@gmail.com>
2023-06-21 10:11:07 +0900

    完成

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,6 @@ import "./App.css";
 import TodoInput from "./components/todo/TodoInput";
 import TodoList from "./components/todo/TodoList";
 
-//ストアにstate[]を登録
-//TODOはローカルストレージのオブジェクトを初期値とする
-//TODOは作成と削除のみ
-//inputエリアと表示エリアを別コンポーネントとして作成してグローバルに使用してみる
-//
-//
-//
-//
-//
-//
-//
-
 function App() {
   return (
     <>
